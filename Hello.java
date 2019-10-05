8db8b2bb3b7d5e850b676754a6d0f0479636e687
sndy35 <sndy35@gmail.com>
2019-10-05 19:14:43 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
   public static void main(String args[]) {
-    System.out.println("hello world...trigger jenkins");
+    System.out.println("hello world...trigger jenkins  via scm");
 }
 }
